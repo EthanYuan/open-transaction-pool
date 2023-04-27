aafc62318310b0562c7ff796bf90fcdcf4062c49
ian <me@iany.me>
2023-04-27 10:56:26 +0800

    add skeleton for p2p relayer protocol

--- otx-pool/src/built_in_plugin/p2p_relayer/mod.rs
+++ otx-pool/src/built_in_plugin/p2p_relayer/mod.rs
@@ -1,7 +1,6 @@
 mod p2p;
 
 use std::path::PathBuf;
-use std::thread::{self, JoinHandle};
 
 use crate::plugin::{
     host_service::ServiceHandler,
@@ -10,10 +9,8 @@ use crate::plugin::{
 };
 use anyhow::Result;
 use ckb_async_runtime::Handle as RuntimeHandle;
-use ckb_types::core::service::Request;
-use crossbeam_channel::{bounded, select, unbounded};
-use otx_format::jsonrpc_types::OpenTransaction;
-use otx_plugin_protocol::{MessageFromHost, MessageFromPlugin, PluginInfo};
+use crossbeam_channel::{bounded, unbounded};
+use otx_plugin_protocol::PluginInfo;
 use utils::config::built_in_plugins::P2PRelayerConfig;
 
 pub struct P2PRelayer {
@@ -21,20 +18,12 @@ pub struct P2PRelayer {
     state: PluginState,
     request_handler: RequestHandler,
     msg_handler: MsgHandler,
-    _thread: JoinHandle<()>,
-}
-
-#[derive(Clone)]
-struct Context {
-    plugin_name: String,
-    config: P2PRelayerConfig,
-    service_handler: ServiceHandler,
 }
 
 impl P2PRelayer {
     pub fn new(
         runtime_handle: &RuntimeHandle,
-        service_handler: ServiceHandler,
+        _service_handler: ServiceHandler,
         config: P2PRelayerConfig,
     ) -> Result<Self> {
         log::info!("P2PRelayer started with config: {:?}", config);
@@ -42,90 +31,37 @@ impl P2PRelayer {
         let state = PluginState::new(PathBuf::default(), true, true);
         let info = PluginInfo::new(name, "This plugin relays OTXs via P2P network.", "1.0");
 
-        let (msg_handler, request_handler, thread) = P2PRelayer::start_process(
-            runtime_handle,
-            Context::new(name.to_owned(), config, service_handler),
-        )?;
+        let (msg_handler, request_handler) = P2PRelayer::start_process(runtime_handle, config)?;
         Ok(P2PRelayer {
             state,
             info,
             msg_handler,
             request_handler,
-            _thread: thread,
         })
     }
 
     fn start_process(
         runtime_handle: &RuntimeHandle,
-        context: Context,
-    ) -> Result<(MsgHandler, RequestHandler, JoinHandle<()>)> {
+        config: P2PRelayerConfig,
+    ) -> Result<(MsgHandler, RequestHandler)> {
         // the host request channel receives request from host to plugin
         let (host_request_sender, host_request_receiver) = bounded(1);
         // the channel sends notifications or responses from the host to plugin
         let (host_msg_sender, host_msg_receiver) = unbounded();
 
-        let plugin_name = context.plugin_name.to_owned();
+        // TODO: broadcast new tx
 
-        if context.config.listen().is_some() {
-            p2p::server(runtime_handle);
-        } else {
-            p2p::client(runtime_handle);
+        let mut p2p_builder = p2p::builder();
+        if let Some(listen) = config.listen() {
+            p2p_builder.listen(listen);
+        }
+        if let Some(dial) = config.dial() {
+            p2p_builder.dial(dial);
         }
 
-        // this thread processes information from host to plugin
-        let thread = thread::spawn(move || {
-            let do_select = || -> Result<bool, String> {
-                select! {
-                    // request from host to plugin
-                    recv(host_request_receiver) -> msg => {
-                        match msg {
-                            Ok(Request { responder, arguments }) => {
-                                log::debug!("{} receives request arguments: {:?}",
-                                    context.plugin_name, arguments);
-                                // handle
-                                let response = (0, MessageFromPlugin::Ok);
-                                responder.send(response).map_err(|err| err.to_string())?;
-                                Ok(false)
-                            }
-                            Err(err) => Err(err.to_string())
-                        }
-                    }
-                    // repsonse/notification from host to plugin
-                    recv(host_msg_receiver) -> msg => {
-                        match msg {
-                            Ok(msg) => {
-                                match msg {
-                                    (_, MessageFromHost::NewInterval(_)) => {
-                                    }
-                                    (_, MessageFromHost::NewOtx(otx)) => {
-                                        on_new_open_tx(context.clone(), otx);
-                                    }
-                                    (_, MessageFromHost::CommitOtx(_)) => {
-                                    }
-                                    _ => unreachable!(),
-                                }
-                                Ok(false)
-                            }
-                            Err(err) => Err(err.to_string())
-                        }
-                    }
-                }
-            };
-            loop {
-                match do_select() {
-                    Ok(true) => {
-                        break;
-                    }
-                    Ok(false) => (),
-                    Err(err) => {
-                        log::error!("plugin {} error: {}", plugin_name, err);
-                        break;
-                    }
-                }
-            }
-        });
+        p2p_builder.spawn(runtime_handle, host_request_receiver, host_msg_receiver);
 
-        Ok((host_msg_sender, host_request_sender, thread))
+        Ok((host_msg_sender, host_request_sender))
     }
 }
 
@@ -149,25 +85,4 @@ impl Plugin for P2PRelayer {
     fn get_state(&self) -> PluginState {
         self.state.clone()
     }
-}
-
-impl Context {
-    pub fn new(
-        plugin_name: String,
-        config: P2PRelayerConfig,
-        service_handler: ServiceHandler,
-    ) -> Self {
-        Self {
-            plugin_name,
-            config,
-            service_handler,
-        }
-    }
-}
-
-fn on_new_open_tx(context: Context, otx: OpenTransaction) {
-    log::trace!("{:?}", context.plugin_name);
-    log::trace!("{:?}", context.config);
-    log::trace!("{:?}", context.service_handler);
-    log::debug!("{:?}", otx);
 }
--- otx-pool/src/built_in_plugin/p2p_relayer/p2p.rs
+++ otx-pool/src/built_in_plugin/p2p_relayer/p2p.rs
@@ -1,59 +1,40 @@
-use bytes::Bytes;
 use ckb_async_runtime::Handle as RuntimeHandle;
-use futures::{
-    channel::oneshot::{channel, Sender},
-    future::select,
-    prelude::*,
-};
-use std::collections::HashMap;
-use std::{str, time::Duration};
+use ckb_types::core::service::Request;
+use crossbeam_channel::{select, Receiver};
+use otx_plugin_protocol::{MessageFromHost, MessageFromPlugin};
+use std::str;
 use tentacle::{
     async_trait,
     builder::{MetaBuilder, ServiceBuilder},
     context::{ProtocolContext, ProtocolContextMutRef, ServiceContext},
     secio::SecioKeyPair,
-    service::{
-        ProtocolHandle, ProtocolMeta, Service, ServiceError, ServiceEvent, TargetProtocol,
-        TargetSession,
-    },
+    service::{ProtocolHandle, ProtocolMeta, ServiceError, ServiceEvent, TargetProtocol},
     traits::{ServiceHandle, ServiceProtocol},
     ProtocolId, SessionId,
 };
 
 // Any protocol will be abstracted into a ProtocolMeta structure.
 // From an implementation point of view, tentacle treats any protocol equally
-fn create_meta(id: ProtocolId) -> ProtocolMeta {
+fn create_relayer_meta(id: ProtocolId) -> ProtocolMeta {
     MetaBuilder::new()
         .id(id)
         .service_handle(move || {
             // All protocol use the same handle.
             // This is just an example. In the actual environment, this should be a different handle.
-            let handle = Box::new(PHandle {
-                count: 0,
-                connected_session_ids: Vec::new(),
-                clear_handle: HashMap::new(),
-            });
+            let handle = Box::new(RelayerProtocol::default());
             ProtocolHandle::Callback(handle)
         })
         .build()
 }
 
 #[derive(Default)]
-struct PHandle {
-    count: usize,
+struct RelayerProtocol {
     connected_session_ids: Vec<SessionId>,
-    clear_handle: HashMap<SessionId, Sender<()>>,
 }
 
 #[async_trait]
-impl ServiceProtocol for PHandle {
-    async fn init(&mut self, context: &mut ProtocolContext) {
-        if context.proto_id == 0.into() {
-            let _ = context
-                .set_service_notify(0.into(), Duration::from_secs(5), 3)
-                .await;
-        }
-    }
+impl ServiceProtocol for RelayerProtocol {
+    async fn init(&mut self, _context: &mut ProtocolContext) {}
 
     async fn connected(&mut self, context: ProtocolContextMutRef<'_>, version: &str) {
         let session = context.session;
@@ -67,36 +48,6 @@ impl ServiceProtocol for PHandle {
             version
         );
         log::info!("connected sessions are: {:?}", self.connected_session_ids);
-
-        if context.proto_id != 1.into() {
-            return;
-        }
-
-        // Register a scheduled task to send data to the remote peer.
-        // Clear the task via channel when disconnected
-        let (sender, receiver) = channel();
-        self.clear_handle.insert(session.id, sender);
-        let session_id = session.id;
-        let interval_sender = context.control().clone();
-
-        let interval_send_task = async move {
-            let mut interval =
-                tokio::time::interval_at(tokio::time::Instant::now(), Duration::from_secs(5));
-            loop {
-                interval.tick().await;
-                let _ = interval_sender
-                    .send_message_to(session_id, 1.into(), Bytes::from("I am a interval message"))
-                    .await;
-            }
-        };
-
-        let task = select(receiver, interval_send_task.boxed());
-
-        let _ = context
-            .future_task(async move {
-                task.await;
-            })
-            .await;
     }
 
     async fn disconnected(&mut self, context: ProtocolContextMutRef<'_>) {
@@ -108,10 +59,6 @@ impl ServiceProtocol for PHandle {
             .collect();
         self.connected_session_ids = new_list;
 
-        if let Some(handle) = self.clear_handle.remove(&context.session.id) {
-            let _ = handle.send(());
-        }
-
         log::info!(
             "proto id [{}] close on session [{}]",
             context.proto_id,
@@ -120,104 +67,132 @@ impl ServiceProtocol for PHandle {
     }
 
     async fn received(&mut self, context: ProtocolContextMutRef<'_>, data: bytes::Bytes) {
-        self.count += 1;
         log::info!(
-            "received from [{}]: proto [{}] data {:?}, message count: {}",
+            "received from [{}]: proto [{}] data {:?}",
             context.session.id,
             context.proto_id,
             str::from_utf8(data.as_ref()).unwrap(),
-            self.count
         );
-    }
-
-    async fn notify(&mut self, context: &mut ProtocolContext, token: u64) {
-        log::info!(
-            "proto [{}] received notify token: {}",
-            context.proto_id,
-            token
-        );
+        // TODO(now): deserialize data
+        // TODO(now): send the received otx to pool
     }
 }
 
-struct SHandle;
+struct P2PServiceHandle;
 
 #[async_trait]
-impl ServiceHandle for SHandle {
-    // A lot of internal error events will be output here, but not all errors need to close the service,
-    // some just tell users that they need to pay attention
+impl ServiceHandle for P2PServiceHandle {
     async fn handle_error(&mut self, _context: &mut ServiceContext, error: ServiceError) {
         log::info!("service error: {:?}", error);
     }
-    async fn handle_event(&mut self, context: &mut ServiceContext, event: ServiceEvent) {
+    async fn handle_event(&mut self, _context: &mut ServiceContext, event: ServiceEvent) {
         log::info!("service event: {:?}", event);
-        if let ServiceEvent::SessionOpen { .. } = event {
-            let delay_sender = context.control().clone();
-
-            let _ = context
-                .future_task(async move {
-                    tokio::time::sleep_until(tokio::time::Instant::now() + Duration::from_secs(3))
-                        .await;
-                    let _ = delay_sender
-                        .filter_broadcast(
-                            TargetSession::All,
-                            0.into(),
-                            Bytes::from("I am a delayed message"),
-                        )
-                        .await;
-                })
-                .await;
-        }
     }
 }
 
-// A p2p application is a service. During the construction process,
-// all protocols supported by the application need to be registered in the way of meta.
-// There are many other options in service builder, which are not used here, please refer to the documentation for details
-//
-// For the foreseeable future, there is no idea of dynamic addition and deletion agreements
-fn create_server() -> Service<SHandle> {
-    ServiceBuilder::default()
-        .insert_protocol(create_meta(0.into()))
-        .insert_protocol(create_meta(1.into()))
-        .key_pair(SecioKeyPair::secp256k1_generated())
-        .build(SHandle)
+#[derive(Default)]
+pub struct P2PBuilder {
+    listen_address: Option<String>,
+    dial_address: Option<String>,
 }
 
-/// Proto 0 open success
-/// Proto 1 open success
-/// Proto 2 open failure
-///
-/// Because server only supports 0,1
-fn create_client() -> Service<SHandle> {
-    ServiceBuilder::default()
-        .insert_protocol(create_meta(0.into()))
-        .insert_protocol(create_meta(1.into()))
-        .insert_protocol(create_meta(2.into()))
-        .key_pair(SecioKeyPair::secp256k1_generated())
-        .build(SHandle)
+impl P2PBuilder {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn listen(self: &mut Self, address: &str) {
+        self.listen_address = Some(address.to_owned());
+    }
+
+    pub fn dial(self: &mut Self, address: &str) {
+        self.dial_address = Some(address.to_owned());
+    }
+
+    pub fn spawn(
+        self: Self,
+        handle: &RuntimeHandle,
+        request_receiver: Receiver<Request<(u64, MessageFromHost), (u64, MessageFromPlugin)>>,
+        msg_receiver: Receiver<(u64, MessageFromHost)>,
+    ) {
+        // TODO(later): use async channel
+        handle.spawn_blocking(move || {
+            let do_select = || -> Result<bool, String> {
+                select! {
+                    // request from host to plugin
+                    recv(request_receiver) -> msg => {
+                        match msg {
+                            Ok(Request { responder, arguments }) => {
+                                log::debug!("p2p_relayer receives request arguments: {:?}", arguments);
+                                let response = (0, MessageFromPlugin::Ok);
+                                responder.send(response).map_err(|err| err.to_string())?;
+                                Ok(false)
+                            }
+                            Err(err) => Err(err.to_string())
+                        }
+                    }
+                    // repsonse/notification from host to plugin
+                    recv(msg_receiver) -> msg => {
+                        match msg {
+                            Ok(msg) => {
+                                match msg {
+                                    (_, MessageFromHost::NewInterval(_)) => {
+                                    }
+                                    (_, MessageFromHost::NewOtx(otx)) => {
+                                        log::info!("p2p_relayer receivers msg NewOtx hash: {:?}", otx.get_tx_hash().expect("get tx hash"));
+                                        // TODO(now): broadcast otx
+                                    }
+                                    (_, MessageFromHost::CommitOtx(_)) => {
+                                    }
+                                    _ => unreachable!(),
+                                }
+                                Ok(false)
+                            }
+                            Err(err) => Err(err.to_string())
+                        }
+                    }
+                }
+            };
+            loop {
+                match do_select() {
+                    Ok(true) => {
+                        break;
+                    }
+                    Ok(false) => (),
+                    Err(err) => {
+                        log::error!("plugin p2p_relayer error: {}", err);
+                        break;
+                    }
+                }
+            }
+        });
+
+        // TODO(later): graceful shutdown
+        handle.spawn(async {
+            let mut service = ServiceBuilder::default()
+                .insert_protocol(create_relayer_meta(0.into()))
+                .key_pair(SecioKeyPair::secp256k1_generated())
+                .build(P2PServiceHandle);
+            if let Some(address) = self.listen_address {
+                service
+                    .listen(address.parse().expect("valid listen address"))
+                    .await
+                    .expect("listen ok");
+            }
+            if let Some(address) = self.dial_address {
+                service
+                    .dial(
+                        address.parse().expect("valid dial address"),
+                        TargetProtocol::All,
+                    )
+                    .await
+                    .expect("dial ok");
+            }
+            service.run().await
+        });
+    }
 }
 
-pub(crate) fn server(handle: &RuntimeHandle) {
-    handle.spawn(async {
-        let mut service = create_server();
-        service
-            .listen("/ip4/127.0.0.1/tcp/1337".parse().unwrap())
-            .await
-            .unwrap();
-        service.run().await
-    });
-}
-
-pub(crate) fn client(handle: &RuntimeHandle) {
-    handle.spawn(async {
-        let mut service = create_client();
-        service
-            .dial(
-                "/ip4/127.0.0.1/tcp/1337".parse().unwrap(),
-                TargetProtocol::All,
-            )
-            .await
-            .unwrap();
-        service.run().await
-    });
+pub fn builder() -> P2PBuilder {
+    P2PBuilder::new()
 }
